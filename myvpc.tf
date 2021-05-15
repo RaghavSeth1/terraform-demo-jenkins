9c2803f33e884f96d94eb0183d9ab40de8918e6d
Raghav Seth <raghav.seth001@gmail.com>
2021-05-15 15:39:13 +0530

    Jenkins-Terraform-EC2Role

---
+++ myvpc.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+
+}
+
+#Tasks:
+
+#Create VPC
+resource "aws_vpc" "prod-vpc" {
+  
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    "Name" = "production"
+  }
+}
